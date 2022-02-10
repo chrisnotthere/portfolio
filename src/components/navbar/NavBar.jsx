504652f7ec10cf9492e0d5f0f9aa7157ef4dd0a8
chris <mr.chrisnash@gmail.com>
2022-02-10 09:58:36 -0800

    menu improvements

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,26 +6,36 @@ const NavBar = () => {
   return (
     <nav class="navbar">
       <ul className='nav-list hamburger'>
-        <li className='nav-item' >
+        <li className='nav-link' >
           <img className='nav-icon' src={More} alt="" />
         </li>
       </ul>
       <ul class="nav-list default">
-        <li className="nav-item">
-          <a href="#home" className="nav-link">Home</a>
-        </li>
-        <li className="nav-item">
-          <a href="#about" className="nav-link">About</a>
-        </li>
-        <li className="nav-item">
-          <a href="#projects" className="nav-link">Projects</a>
-        </li>
-        <li className="nav-item">
-          <a href="#skills" className="nav-link">Skills</a>
-        </li>
-        <li className="nav-item">
-          <a href="#contact" className="nav-link">Contact</a>
-        </li>
+        <a href="#home" className="nav-link">
+          <li className="nav-item">
+            Home
+          </li>
+        </a>
+        <a href="#about" className="nav-link">
+          <li className="nav-item">
+            About
+          </li>
+        </a>
+        <a href="#projects" className="nav-link">
+          <li className="nav-item">
+            Projects
+          </li>
+        </a>
+        <a href="#skills" className="nav-link">
+          <li className="nav-item">
+            Skills
+          </li>
+        </a>
+        <a href="#contact" className="nav-link">
+          <li className="nav-item">
+            Contact
+          </li>
+        </a>
       </ul>
     </nav>
   )
