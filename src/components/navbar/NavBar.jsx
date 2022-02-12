88a80e35cca1a33738fc1829b9acb2ebfb757b2d
chris <mr.chrisnash@gmail.com>
2022-02-11 16:10:48 -0800

    navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import More from '../../images/more.png'
+import Home from '../../images/home.png'
 
 const NavBar = () => {
   return (
@@ -12,9 +13,9 @@ const NavBar = () => {
       </ul>
       <ul class="nav-list default">
         <a href="#home" className="nav-link">
-          <li className="nav-item">
-            Home
-          </li>
+          {/* <li className="nav-item"> */}
+            <img className='nav-icon' src={Home} alt="" />
+          {/* </li> */}
         </a>
         <a href="#about" className="nav-link">
           <li className="nav-item">
@@ -34,6 +35,11 @@ const NavBar = () => {
         <a href="#contact" className="nav-link">
           <li className="nav-item">
             Contact
+          </li>
+        </a>
+        <a href="https://chrisnotthere.github.io/blog-client/" className="nav-link">
+          <li className="nav-item">
+            Blog
           </li>
         </a>
       </ul>
