a983aa9e088aa9baaf2ddce3f457ed1ee980432e
chris <mr.chrisnash@gmail.com>
2022-02-17 11:54:15 -0800

    about me write up

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
         </div>
       </div>
       <div className="about-right">
-        <h1 className="about-title"><span>About me</span> From cooking to web development.</h1>
+        <h1 className="about-title"><span>About me</span> From the hospitality industry to web development.</h1>
         <p className="about-desc">I'm an ex-cook who loves building things from scratch.</p>
-        <p className="about-desc">I had long considered changing career before the 2020 pandemic, but after losing my job that was the push I needed to make the jump. I immediately began learning the basics though freeCodeCamp, finishing their "Responsive Web Design Certification" and loved every second of it. I then completed an "Interative Media Development" college certificate, where I learned all kinds of things from web development, to 3D modeling in Autodesk Maya, and worked with Procreate and Adobe After Effects. After IMD I have been self-taught, solely focusing on web development. Since then I have finished The Odin Project's Full Stack Javascript curriculum.</p>
-        <p className="about-desc">My time in the hospitality industry has taught me many things, and left me with an abundance transferable skills such as "get-stuff-done", effective communication, a calm head during the lunch-rush, and much more.</p>
+        <p className="about-desc">I had long considered changing career before the 2020 pandemic, but after losing my job that was the push I needed to make the jump. I immediately began learning the basics though freeCodeCamp, finishing their "Responsive Web Design Certification" and loved every second of it. I then completed an "Interactive Media Development" college certificate, where I learned all kinds of things from web development, to making games with Unity/Maya, and UI/UX design principles . After IMD I have been self-taught. Since then I have finished The Odin Project's Full Stack Javascript curriculum.</p>
+        <p className="about-desc">My time in the hospitality industry has taught me many things, and left me with an abundance transferable skills such as "getting-stuff-done", effective communication, a calm head during the lunch-rush, and much more.</p>
         <p className="about-desc">Web development allows me to create things from the ground-up, solve technical problems along the way, and harness my creativity. The blend of development and design is what makes me excited about a future in this field.</p>
         <p className="about-desc">After almost two years of learning, I am ready to level-up to a full-time role where I can contribute to a team while continuing to learn and grow.</p>
       </div>
