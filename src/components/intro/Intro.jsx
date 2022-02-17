413553dfae1f84e65eeb04490efacdd60dbb62d8
chris <mr.chrisnash@gmail.com>
2022-02-17 10:57:27 -0800

    intro card text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './intro.css';
 import Webdev from '../../images/webdev.png';
 import { ThemeContext } from '../../context';
@@ -6,6 +6,57 @@ import { ThemeContext } from '../../context';
 const Intro = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
+
+  function consoleText(words, id, colors) {
+    var visible = true;
+    var con = document.getElementById('console');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' + colors[0])
+    window.setInterval(function () {
+
+      if (letterCount === 0 && waiting === false) {
+        waiting = true;
+        target.innerHTML = words[0].substring(0, letterCount)
+        window.setTimeout(function () {
+          var usedColor = colors.shift();
+          colors.push(usedColor);
+          var usedWord = words.shift();
+          words.push(usedWord);
+          x = 1;
+          target.setAttribute('style', 'color:' + colors[0])
+          letterCount += x;
+          waiting = false;
+        }, 1000)
+      } else if (letterCount === words[0].length + 1 && waiting === false) {
+        waiting = true;
+        window.setTimeout(function () {
+          x = -1;
+          letterCount += x;
+          waiting = false;
+        }, 1000)
+      } else if (waiting === false) {
+        target.innerHTML = words[0].substring(0, letterCount)
+        letterCount += x;
+      }
+    }, 140)
+    window.setInterval(function () {
+      if (visible === true) {
+        con.className = 'console-underscore hidden'
+        visible = false;
+      } else {
+        con.className = 'console-underscore'
+        visible = true;
+      }
+    }, 400)
+  }
+
+  useEffect(()=> {
+    // function([string1, string2],target id,[color1,color2])    
+    consoleText(['Developer', 'Designer', 'Life-long learner'], 'text', ['#4d9078', '#e26729', '#6596ce']);
+  }, [])
 
   return (
     <div className='intro' id='home' >
@@ -15,11 +66,16 @@ const Intro = () => {
           <div className="intro-greeting">Hello, my name is</div>
           <div className="intro-name">Chris Nash</div>
           <div className="intro-title">
-            <div className="intro-title-wrapper">
+
+            <div className='console-container'>
+              <span id='text'></span>
+              <div className='console-underscore' id='console'>&#95;</div>
+            </div>
+            {/* <div className="intro-title-wrapper">
               <div className="intro-title-item i1">Developer</div>
               <div className="intro-title-item i2">Designer</div>
               <div className="intro-title-item i3">Student</div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
